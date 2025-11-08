bbe509264b1f607143c35edf0023c8ffc36ab10c
farshid-azady <mcpd.developer@gmail.com>
2025-11-09 02:59:41 +0330

    adding one dynamic function in urls

--- monthly_challenges/chalenges/urls.py
+++ monthly_challenges/chalenges/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('junuary', views.junuary),
-    path('february', views.february),
-    # path('march', views.index),
-    # path('april', views.index),
-    # path('may', views.index),
-    # path('june', views.index),
-    # path('july', views.index),
-    # path('august', views.index),
-    # path('september', views.index),
-    # path('october', views.index),
-    # path('november', views.index),
-    # path('december', views.index),
+    path('<month>', views.month, name='month'),
+    
 ]
--- monthly_challenges/chalenges/views.py
+++ monthly_challenges/chalenges/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
@@ -7,6 +7,20 @@ from django.http import HttpResponse
 def junuary(request):
 
     return HttpResponse("<h1>Eat no Meat through Junuary</h1>")
+
+
 def february(request):
 
     return HttpResponse("<h1>walk 20 minute through february</h1>")
+
+
+def month(request, month):
+    challenge_text = {
+        "january": "eat no meat",
+        "february": "walk 20 minute",
+    }
+
+    if month in challenge_text:
+        return HttpResponse(f"<h1 style='color:red'>{challenge_text[month]}</h1>")
+    else:
+        return HttpResponseNotFound("<h1>invalid month</h1>")
